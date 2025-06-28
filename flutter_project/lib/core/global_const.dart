2d6b3dea8e9ec1ac99ddb64b137dbbf771428433
Bastien Mosse <bastien.mosse@epita.fr>
2025-06-28 16:08:40 +0200

    fix: update global

--- flutter_project/lib/core/global_const.dart
+++ flutter_project/lib/core/global_const.dart
@@ -2,21 +2,8 @@ import '/index.dart';
 
 // Application-wide constant variables
 const String appName = 'Stora';
-const String apiBaseUrl = 'https://api.example.com';
+const String apiBaseUrl = 'http://10.0.2.2:5432';
 const int defaultTimeout = 30;
-
-// Theme constants
-const Color darkPrimary = Color(0xFF202020);
-const Color darkSecondary = Color(0xFF161616);
-const Color darkWidget = Color(0xFF2563EB);
-const Color darkText = Color(0xFF1E2533);
-const Color darkTextDark = Color(0xFFFFFFFF);
-
-const Color lightPrimary = Color(0xFFFFFFFF);
-const Color lightSecondary = Color(0xFFF0F0F0);
-const Color lightWidget = Color(0xFF2563EB);
-const Color lightText = Color(0xFF1E2533);
-const Color lightTextLight = Color(0xFF000000);
 
 // Font constants
 const String textFont = 'secondary family';
